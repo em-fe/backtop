d9bfdf4c7158e34a3e9dfa31e7313bdb326f7214
honghaifeng <18500893514@163.com>
2019-06-17 15:46:23 +0800

    fix($name) fix backtop name

--- embacktop/.vuepress/config.js
+++ embacktop/.vuepress/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const resolve = p => path.resolve(__dirname, '../', p);
 
-const base = process.env.GH ? '/backtoppc/' : '/';
+const base = process.env.GH ? '/backtop/' : '/';
 
 module.exports = {
   title: '返回顶部',
