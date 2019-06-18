093677b3e5ed77a7fecd678f3951ba9f1044ba71
honghaifeng <18500893514@163.com>
2019-06-18 11:20:31 +0800

    style($complaint) show complaint

--- embacktop/backtoppc/BacktopPc.vue
+++ embacktop/backtoppc/BacktopPc.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="backtop-pc" :class="{'backtop-pc-fadein':showFlag}" >
+  <div class="backtop-pc">
     <div class="backtop-pc-item" @mouseover="enterMethod(complaint)" @mouseout="outMethod(complaint)">
       <img v-show="!complaint.hoverFlag" src="./assets/img/complaint.png" alt="complaintIcon" class="backtop-pc-item-complaint">
       <img v-show="complaint.hoverFlag" src="./assets/img/complaint-hover.png" alt="complaintIcon" class="backtop-pc-item-complaint">
@@ -11,7 +11,7 @@
         <div class="backtop-pc-item-qrcode-right"></div>
       </div>
     </div>
-    <div class="backtop-pc-item"  @click="backTopMethod" @mouseover="enterMethod(backTop)" @mouseout="outMethod(backTop)">
+    <div class="backtop-pc-itemtwo" :class="{'backtop-pc-itemtwo-fadein':showFlag}" @click="backTopMethod" @mouseover="enterMethod(backTop)" @mouseout="outMethod(backTop)">
       <img v-show="!backTop.hoverFlag" src="./assets/img/backtop.png" alt="backTopIcon" class="backtop-pc-item-backTop">
       <img v-show="backTop.hoverFlag" src="./assets/img/backtop-hover.png" alt="complaintIcon" class="backtop-pc-item-backTop">
       <div>{{topText}}</div>
@@ -68,7 +68,17 @@ export default {
         if (needScrollTop > 10 || needScrollTop < -10) {
           this.scrollAnimation(currentScrollY, targetY);
         } else {
-          window.scrollTo(currentScrollY, targetY);
+          let timer = null;
+          //window.scrollTo(currentScrollY, targetY);
+          //感觉卡顿，再加一个过渡
+          timer = setInterval(() => {
+            const osTop = document.documentElement.scrollTop || document.body.scrollTop;
+            const ispeed = Math.floor(-osTop / 7);
+            window.scrollTo(osTop, (osTop + ispeed));
+            if (osTop === 0) {
+              clearInterval(timer);
+            }
+          }, 30);
         }
       }, 10);
     },
